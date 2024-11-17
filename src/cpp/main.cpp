d60d71101e3b5b6d0cc95961d3b3767bfb750df8
JimmyHu <60071jimmy@gmail.com>
2024-11-17 21:48:44 +0800

    Update main.cpp
    Update main.cpp

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -19,6 +19,7 @@ std::vector<openstl::Triangle> stl_processor(std::vector<openstl::Triangle>& inp
     float maximum_z = input[0].v0.z;
     float maximum_location_x = input[0].v0.x, maximum_location_y = input[0].v0.y;
     for (std::size_t index = 0; index < input.size(); ++index)
+    {
 }
 
 int main()

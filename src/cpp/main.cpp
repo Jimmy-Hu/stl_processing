036fca00426e73779a5cd2f353633120f2886ee8
JimmyHu <60071jimmy@gmail.com>
2024-11-27 21:44:42 +0800

    Update main.cpp
    Update main.cpp

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -29,6 +29,7 @@ std::vector<openstl::Triangle> stl_processor(std::vector<openstl::Triangle>& inp
     }
     std::cout << "maximum_z = " << maximum_z << "\n";
     std::cout << "maximum_location_x = " << maximum_location_x << "\n";
+    std::cout << "maximum_location_y = " << maximum_location_y << "\n";
 }
 
 int main()

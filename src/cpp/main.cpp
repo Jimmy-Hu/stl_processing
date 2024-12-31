19f255f8403eefb7066a39c814ae517f759f4ed9
JimmyHu <60071jimmy@gmail.com>
2024-12-31 18:39:26 +0800

    Update main.cpp
    Update main.cpp

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -65,6 +65,7 @@ auto find_min_x(openstl::Triangle& input)
 auto find_max_y(openstl::Triangle& input)
 {
     float maximum = std::max(input.v0.y, input.v1.y);
+    maximum = std::max(maximum, input.v2.y);
 int main()
 {
     auto start = std::chrono::system_clock::now();

0cf36132cda9f7b038ad7183264cf998871091a7
JimmyHu <60071jimmy@gmail.com>
2025-02-19 14:06:25 +0800

    Update main.cpp
    Update main.cpp

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -115,6 +115,7 @@ auto find_max(std::vector<openstl::Triangle>& input, std::size_t channel)
                 maximum = find_max_y(element);
             }
         }
+        return maximum;
 int main()
 {
     auto start = std::chrono::system_clock::now();

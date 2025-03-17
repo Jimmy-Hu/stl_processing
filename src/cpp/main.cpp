8bf6426961d788a094bb9a7c2be55f07820b6f9d
JimmyHu <60071jimmy@gmail.com>
2025-03-17 12:43:50 +0800

    Update main.cpp
    Update main.cpp

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -141,6 +141,7 @@ auto find_min(std::vector<openstl::Triangle>& input, std::size_t channel)
     case 0:
         minimum = input[0].v0.x;
         for (auto&& element : input)
+        {
 int main()
 {
     auto start = std::chrono::system_clock::now();

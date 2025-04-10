1b97fdb952bc754c241d392883f1335d2dceca5c
JimmyHu <60071jimmy@gmail.com>
2025-04-10 12:27:00 +0800

    Update main.cpp
    Update main.cpp

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -165,6 +165,7 @@ auto find_min(std::vector<openstl::Triangle>& input, std::size_t channel)
         for (auto&& element : input)
         {
             if (minimum > find_min_z(element))
+            {
 int main()
 {
     auto start = std::chrono::system_clock::now();
